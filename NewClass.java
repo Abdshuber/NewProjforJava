a37341a965ddd295ca4023034555925befcddc0c
@abdshuber <94958588+Abdshuber@users.noreply.github.com>
2021-12-12 22:33:09 +0200

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,45 @@
+
+package tester;
+
+import java.util.Scanner;
+
+public class NewClass {
+      public static void main(String[] args) {
+        Scanner in=new Scanner (System .in);
+  
+     Scanner scanner = new Scanner(System.in);
+int amount = scanner.nextInt();
+double sum1,sum2,sum3=0; int  num ;double avarege1,avarege2,avarege3=0;
+  System.out.println("Enter the amount  ="); 
+for ( int i=1 ; i <=1; i ++) {
+       
+sum1=(amount-( amount* 0.1));
+  System.out.println("Payment : "+amount*.1);    
+System.out.println("Remaining amount :"+Math.round(sum1));   
+
+      sum2=(sum1-(sum1* 0.1));
+      
+      System.out.println("Payment :"+Math.round(sum1*.1));
+     
+      System.out.println("Remaining amount :"+Math.round(sum2));
+  
+      sum3=(sum2-(sum2 * 0.1));
+      System.out.println("Payment :"+Math.round(sum2*0.1));
+     
+
+          System.out.println( Math.round(sum3));  
+      }
+
+     
+
+}
+
+          
+          
+          
+      }
+    
+    
+    
+
+
